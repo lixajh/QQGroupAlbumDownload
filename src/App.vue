ffa14bb83b9387f0cba0ce30192c09d2a9fb6cf8
lihengdao <4548212@qq.com>
2024-12-25 11:25:05 +0800

    element-ui组件配置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
+
 </template>
 
 <style lang="scss">
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
-
+const AutoImport = require('unplugin-auto-import/webpack').default
+const Components = require('unplugin-vue-components/webpack').default
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+console.log(AutoImport)
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './' 
+  publicPath: './' ,
+  configureWebpack:{
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  }
 });
