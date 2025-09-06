2253ca7a0bee7eb9ae57bd0f691d7373f3f4f263
lixajh <lixajh@gmail.com>
2025-09-06 14:41:13 +0800

    change downlod dir

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // 配置文件
 const config = {
   // QQ群号
-  qqGroupNumber: '792054520', // 默认值，用户需要修改为实际的群号
+  qqGroupNumber: '792054520', // 默认值，用户需要修改为实际的群号  
   
-  // 下载文件夹地址
-  downloadPath: 'QQdownload' // 默认值，用户需要修改为实际的下载路径
+  // 下载文件夹地址 - 修改为桌面路径，避免electronmon检测到文件变化导致页面重加载
+  downloadPath: require('path').join(require('os').homedir(), 'Desktop/QQ群相册下载') // 下载到桌面的QQ群相册下载文件夹
 };
 
 module.exports = config;
